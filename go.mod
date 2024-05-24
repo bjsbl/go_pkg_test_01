85efc0008c2bd061d634938a2d7b1c80a629581c
sbl <baolaing.shen@17smile.cn>
2024-05-24 12:52:17 +0800

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/bjsbl/go_pkg_test_01
+
+go 1.22.2
---
+++ helper.go
@@ -0,0 +1,9 @@
+package go_pkg_test_01
+
+import "fmt"
+
+const version = "1.0.1"
+
+func cliVersion() {
+	fmt.Println(" pkg version " + version)
+}
